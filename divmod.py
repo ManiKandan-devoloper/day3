2238010ac89c204ccaded3d687f8f2cc41fcc55d
ManiKandan-devoloper <81610132+ManiKandan-devoloper@users.noreply.github.com>
2021-08-23 02:58:52 -0400

    Add files via upload

---
+++ divmod.py
@@ -0,0 +1,11 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(divmod(9,3))
+(3, 0)
+>>> print(divmod(2,4))
+(0, 2)
+>>> a=16
+>>> b=4
+>>> print(divmod(a,b))
+(4, 0)
+>>> 
